19f4f8717ca2019eed35a35606af4a28f6048ea6
Philipp Sumi <philipp@hardcodet.net>
2013-11-17 23:47:44 +0000

    CHG   Better ContextMenu tutorial sample.
    git-svn-id: https://svn.evolvesoftware.ch/repos/evolve.net/WPF/NotifyIcon@187 9f600761-6f11-4665-b6dc-0185e9171623

--- Source/Sample Project/Tutorials/04 - ContextMenus/InlineContextMenuWindow.xaml.cs
+++ Source/Sample Project/Tutorials/04 - ContextMenus/InlineContextMenuWindow.xaml.cs
@@ -1,35 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Samples.Tutorials.ContextMenus
 {
-  /// <summary>
-  /// Interaction logic for InlineContextMenuWindow.xaml
-  /// </summary>
-  public partial class InlineContextMenuWindow : Window
-  {
-    public InlineContextMenuWindow()
+    /// <summary>
+    /// Interaction logic for InlineContextMenuWindow.xaml
+    /// </summary>
+    public partial class InlineContextMenuWindow : Window
     {
-      InitializeComponent();
+        public InlineContextMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            //clean up notifyicon (would otherwise stay open until application finishes)
+            MyNotifyIcon.Dispose();
+
+            base.OnClosing(e);
+        }
+
+        private void MyNotifyIcon_TrayContextMenuOpen(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenEventCounter.Text = (int.Parse(OpenEventCounter.Text) + 1).ToString();
+        }
+
+        private void MyNotifyIcon_PreviewTrayContextMenuOpen(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //marking the event as handled suppresses the context menu
+            e.Handled = (bool)SuppressContextMenu.IsChecked;
+
+            PreviewOpenEventCounter.Text = (int.Parse(PreviewOpenEventCounter.Text) + 1).ToString();
+        }
     }
-
-
-    protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
-    {
-      //clean up notifyicon (would otherwise stay open until application finishes)
-      MyNotifyIcon.Dispose();
-
-      base.OnClosing(e);
-    }
-  }
 }
