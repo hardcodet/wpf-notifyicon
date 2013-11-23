3f5fd9284efb598c6dd694ef8c76913cc1fba72c
Philipp Sumi <philipp@hardcodet.net>
2013-11-23 18:22:31 +0000

    FIX   Adding missing sample command.
    git-svn-id: https://svn.evolvesoftware.ch/repos/evolve.net/WPF/NotifyIcon@212 9f600761-6f11-4665-b6dc-0185e9171623

---
+++ Source/Sample Project/Commands/CloseWindowCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Samples.Commands
+{
+    /// <summary>
+    /// Closes the current window.
+    /// </summary>
+    public class CloseWindowCommand : CommandBase<CloseWindowCommand>
+    {
+        public override void Execute(object parameter)
+        {
+            GetTaskbarWindow(parameter).Close();
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+
+        public override bool CanExecute(object parameter)
+        {
+            Window win = GetTaskbarWindow(parameter);
+            return win != null;
+        }
+    }
+}
