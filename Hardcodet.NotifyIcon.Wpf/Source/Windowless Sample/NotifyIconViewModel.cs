9c461c74370b046ac4bc524bf5df36e7b98de1d2
Krom, Robertus <Robertus.Krom@ing.de>
2019-07-16 09:15:44 +0200

    And fixed another

--- Hardcodet.NotifyIcon.Wpf/Source/Windowless Sample/NotifyIconViewModel.cs
+++ Hardcodet.NotifyIcon.Wpf/Source/Windowless Sample/NotifyIconViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 
 namespace Windowless_Sample
@@ -55,32 +54,6 @@ namespace Windowless_Sample
             {
                 return new DelegateCommand {CommandAction = () => Application.Current.Shutdown()};
             }
-        }
-    }
-
-
-    /// <summary>
-    /// Simplistic delegate command for the demo.
-    /// </summary>
-    public class DelegateCommand : ICommand
-    {
-        public Action CommandAction { get; set; }
-        public Func<bool> CanExecuteFunc { get; set; }
-
-        public void Execute(object parameter)
-        {
-            CommandAction();
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return CanExecuteFunc == null  || CanExecuteFunc();
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
